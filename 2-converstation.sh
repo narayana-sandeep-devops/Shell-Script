31a0c1d8d3c43f16abf10dcbc206c48ca9dbaad5
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-06 15:00:53 +0530

    new shell script practice

---
+++ 2-converstation.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "Sandeep: Hello Nandini, How are you?"
+echo "Nandini: Hi Sandeep, I am doing good, how about you?"
+echo "Sandeep: I am doing good, where do you stay Nandini"
+echo "Nandini: I stay in Bangalore sandeep"
