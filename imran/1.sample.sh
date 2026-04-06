2201b88e426f6f9dc61dc32e235dc3023ebab2db
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-06 21:44:22 +0530

    new imran folder created for practice

---
+++ imran/1.sample.sh
@@ -0,0 +1,13 @@
+# https://visualpath.in/devopstutorials/devops
+#!/bin/bash
+
+echo "Welcome to bash script"
+echo
+echo "The uptime of the system is:"
+uptime
+
+echo "Memory Utilization:"
+free -m
+
+echo "Disk Utilization"
+df -h
