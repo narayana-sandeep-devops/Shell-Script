7bc9a22a6c4417e86a522414600a151314015bd4
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-06 16:42:37 +0530

    update variable

--- 6-variables.sh
+++ 6-variables.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-TIMESTAMP={date}
+START_TIME=$(date +%s)
 
 echo "Script executed at: $TIMESTAMP"
+
+sleep 10
+
+END_TIME=$(date +%s)
+echo "Script end Time: $END_TIME"
+
+TOTAL_TIME=$((END_TIME-$START_TIME))
+
+echo "Total time of script execution: $TOTAL_TIME"
