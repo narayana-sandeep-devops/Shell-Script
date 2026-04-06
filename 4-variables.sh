a5a39f84fa9b277fddd5d770c16c7bdb8caf55ec
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-06 16:12:55 +0530

    update variable

---
+++ 4-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+P1=$1
+P2=$2
+
+echo "$P1: Hello $P2, How are you?"
+echo "$P2: Hi $P1, I am doing good, how about you?"
+echo "$P1: I am doing good, where do you stay $P2"
+echo "$P2: I stay in Bangalore $P1"
