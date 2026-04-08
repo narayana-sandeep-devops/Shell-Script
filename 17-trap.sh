4debeefedfce50b204ca6bda3ca06ef044f26feb
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-08 21:37:34 +0530

    new files updated

--- 17-trap.sh
+++ 17-trap.sh
@@ -2,7 +2,7 @@
 
 set -e #ERR
 
-trap 'echo "There is an error in $LINENO, command: $BASH)COMMAND"' ERR
+trap 'echo "There is an error in $LINENO, command: $BASH_COMMAND"' ERR
 
 echo "Hello World"
 echo "I am learning Shell"
