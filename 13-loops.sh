b2bc08449ff4e89ff6bf3beb72739c64dfa8ccdd
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-08 21:09:16 +0530

    new file created/modified

---
+++ 13-loops.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this command with root user:" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2: Failure" | tee -a $LOG_FILE
+    else
+        echo "$2: Success" | tee -a $LOG_FILE
+    fi
+}
+
+
+for package in $@
+do
+    dnf install $package -y
+    VALIDATE $? "$package installation"
