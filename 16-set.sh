4c39198c8b128396c93cab5da6a2cec4ae86a9b0
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-08 21:30:22 +0530

    new files updated

--- 16-set.sh
+++ 16-set.sh
@@ -30,7 +30,7 @@ VALIDATE(){
 
 for package in $@
 do
-    dnf list installed $package %>> $LOG_FILE
+    dnf list installed $package &>> $LOG_FILE
     if [ $? -ne 0 ]; then
         echo "$package not installed, installing now"
         dnf install $package -y
