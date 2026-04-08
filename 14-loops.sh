55b9833ec6b50c075eb18bcae6b0e04b177e5095
Sandeep Narayana <sandeep.narayana2013@outlook.com>
2026-04-08 21:16:42 +0530

    new files updated

---
+++ 14-loops.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this command with root user:" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2: Failure" | tee -a $LOG_FILE
+    else
+        echo "$2: Success" | tee -a $LOG_FILE
+    fi
+}
+
+
+for package in $@
+do
+    dnf list installed $package %>> $LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y
+        VALIDATE $? "$package installation"
+    else
+        echo "$package already installed, skipping"
+    fi
+done
